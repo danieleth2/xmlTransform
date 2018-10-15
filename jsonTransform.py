da94a49757292ed3a41b4b78ebcf10d6de0147fe
8hfq <w@8hfq.com>
2018-10-15 16:30:19 +0800

    clean

--- jsonTransform.py
+++
@@ -1,8 +0,0 @@
-import json
-from collections import OrderedDict
-s = '{"dataset":{"1":{"data-source-type":"NATIVE","label":"医院列表数据","parameters":{"regionCode":490.1,"limit":2334}},"2":{"data-source-type":"NATIVE","label":"医院列表数据","parameters":{"regionCode":123,"limit":124345}}}}'
-
-
-data = json.loads(s, object_pairs_hook=OrderedDict)
-
-print(data["dataset"]["1"]["label"])
